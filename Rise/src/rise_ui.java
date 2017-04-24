8cf34165364b3a5da62180e3368a3d8dc7af253f
KeinZeit <KeinZeit@147.8.60.17>
2017-04-25 02:06:13 +0800

    added text fields

--- Rise/src/rise_ui.java
+++ Rise/src/rise_ui.java
@@ -3,10 +3,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.GridLayout;
 import javax.swing.JButton;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLabel;
 
 public class rise_ui {
 
 	private JFrame frame;
+	private JTextField txtUsername;
+	private JTextField txtPassword;
 
 	/**
 	 * Launch the application.
@@ -36,12 +45,40 @@ public class rise_ui {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 270, 480);
+		frame.setBounds(100, 100, 360, 640);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
 		
-		JButton btnHello = new JButton("hello");
-		frame.getContentPane().add(btnHello);
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, "login_page");
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{95, 80, 80, 95};
+		gbl_panel.rowHeights = new int[]{68, 147, 40, 40, 40, 40, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		txtUsername = new JTextField();
+		GridBagConstraints gbc_txtUsername = new GridBagConstraints();
+		gbc_txtUsername.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtUsername.insets = new Insets(0, 0, 5, 5);
+		gbc_txtUsername.gridx = 1;
+		gbc_txtUsername.gridy = 2;
+		gbc_txtUsername.gridwidth = 2;  // columns that the textfield stretches over
+		gbc_txtUsername.gridheight = 2; // rows that ....
+		panel.add(txtUsername, gbc_txtUsername);
+		txtUsername.setColumns(10);
+		
+		txtPassword = new JTextField();
+		GridBagConstraints gbc_txtPassword = new GridBagConstraints();
+		gbc_txtPassword.insets = new Insets(0, 0, 5, 5);
+		gbc_txtPassword.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtPassword.gridx = 1;
+		gbc_txtPassword.gridy = 4;
+		gbc_txtPassword.gridwidth = 2;
+		gbc_txtPassword.gridheight = 2;
+		panel.add(txtPassword, gbc_txtPassword);
+		txtPassword.setColumns(10);
 	}
 // making a change!!!! here
 }
